fb131f71539b59922606a38dbdaa7338bfab304d
Jishnu.V <its.real.jishnu@gmail.com>
2023-10-15 23:47:57 +0530

    using lombok

--- src/main/java/com/ostn/map/entity/LabelDetails.java
+++ src/main/java/com/ostn/map/entity/LabelDetails.java
@@ -1,13 +1,14 @@
 package com.ostn.map.entity;
 
-
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @Table(name = "label_details")
 @ToString
+@NoArgsConstructor
 public class LabelDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,92 +36,5 @@ public class LabelDetails {
     private Long floorId;
     private String description;
 
-    public LabelDetails() {
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Nonnull
-    public Long getBuildingId() {
-        return buildingId;
-    }
-
-    public void setBuildingId(@Nonnull Long buildingId) {
-        this.buildingId = buildingId;
-    }
-
-    @Nonnull
-    public Long getxCoordinate() {
-        return xCoordinate;
-    }
-
-    public void setxCoordinate(@Nonnull Long xCoordinate) {
-        this.xCoordinate = xCoordinate;
-    }
-
-    @Nonnull
-    public Long getyCoordinate() {
-        return yCoordinate;
-    }
-
-    public void setyCoordinate(@Nonnull Long yCoordinate) {
-        this.yCoordinate = yCoordinate;
-    }
-
-    public Long getFloor() {
-        return floor;
-    }
-
-    public void setFloor(Long floor) {
-        this.floor = floor;
-    }
-
-    @Nonnull
-    public String getLabelName() {
-        return labelName;
-    }
-
-    public void setLabelName(@Nonnull String labelName) {
-        this.labelName = labelName;
-    }
-
-    public String getIconName() {
-        return iconName;
-    }
-
-    public void setIconName(String iconName) {
-        this.iconName = iconName;
-    }
-
-    @Nonnull
-    public Long getLabelId() {
-        return labelId;
-    }
-
-    public void setLabelId(@Nonnull Long labelId) {
-        this.labelId = labelId;
-    }
-
-    @Nonnull
-    public Long getFloorId() {
-        return floorId;
-    }
-
-    public void setFloorId(@Nonnull Long floorId) {
-        this.floorId = floorId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/main/java/com/ostn/map/model/Coordinate.java
+++ src/main/java/com/ostn/map/model/Coordinate.java
@@ -1,24 +1,13 @@
 package com.ostn.map.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Coordinate {
     private double x;
     private double y;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
 
     public Coordinate(double x, double y) {
         this.x = x;
